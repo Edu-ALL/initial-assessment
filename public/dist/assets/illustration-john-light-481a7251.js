a6c72d1dfddb35a3945b69a524f0d271297a19fa
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2024-03-27 11:34:56 +0700

    build

---
+++ public/dist/assets/illustration-john-light-481a7251.js
@@ -0,0 +1,1 @@
+const a="/build/assets/illustration-john-dark-7ad2aad1.png",t="/build/assets/illustration-john-light-0061869a.png";export{t as a,a as i};
--- vite.config.js
+++ vite.config.js
@@ -60,6 +60,7 @@ export default defineConfig({
     },
   },
   build: {
+    outDir: 'public/dist',
     chunkSizeWarningLimit: 5000,
   },
   optimizeDeps: {
