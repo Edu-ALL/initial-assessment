b3dd94b2b1806782afa0131adc0b0d56a2a6c996
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2025-02-05 08:17:31 +0700

    update env

---
+++ public/build/assets/ssrBoot-77966e77.js
@@ -0,0 +1,1 @@
+import{J as s,O as t,s as e,a$ as n}from"./main-7f59cfeb.js";function i(){const o=s(!1);return t(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:e(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
